8fa87b6cbb39154d74aabc56917f45073e1a4534
Abraham3105 <acascantebadilla31@gmail.com>
2025-04-03 15:42:48 -0600

    Cambio

---
+++ back/inventario/src/test/java/com/bodega/inventario/SistemaInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodega.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
